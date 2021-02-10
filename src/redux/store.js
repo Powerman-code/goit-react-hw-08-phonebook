5357d6de6678709cad64a9614ec647b80d0c2ed3
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2021-02-10 18:43:03 +0200

    Errors fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,12 +5,14 @@ import {
 } from '@reduxjs/toolkit';
 import phoneBookReducer from './phoneBook/phoneBook-reducer';
 import logger from 'redux-logger';
+import authReducer from './auth/auth-slice';
 
 const middleware = [...getDefaultMiddleware(), logger];
 
 const store = configureStore({
   reducer: {
     phoneBook: phoneBookReducer,
+    auth: authReducer,
   },
   middleware,
   devTools: process.env.NODE_ENV === 'development',
